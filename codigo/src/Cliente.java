0f684c5dd69dff62576229ad4939de3f56752574
JulyaMoreyra <julia.nascimento.1456172@sga.pucminas.br>
2023-09-04 19:04:05 -0300

    codigo de validação de cpfd

--- codigo/src/Cliente.java
+++ codigo/src/Cliente.java
@@ -20,7 +20,7 @@ public class Cliente {
             throw new Error("erro: nome invalido!");
         }
 
-        if(cpf.length() == 14 && cpf.matches("([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})")) { // teste cpf
+        if(validaCPF(cpf) == true){
             this.cpf = cpf;
         } else {
             throw new Error("erro: cpf invalido!");
@@ -46,7 +46,7 @@ public class Cliente {
         System.out.print("Digite o nome do cliente: ");
         scanner.nextLine();
         String nome = scanner.nextLine();
-        System.out.print("Digite o CPF do cliente: ");
+        System.out.print("Digite o CPF do cliente no formato xxx.xxx.xxx-xx: ");
         String CPF = scanner.nextLine();
         System.out.print("Digite o endereço do cliente: ");
         String endereco = scanner.nextLine();
@@ -93,4 +93,40 @@ public class Cliente {
         }
         return null;
     }
+
+    public static boolean validaCPF(String cpf) {
+        // Verifica se o CPF possui o formato válido
+        String cpfRegex = "^(\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2})|(\\d{11})$";
+        boolean formatoValido = cpf.matches(cpfRegex);
+    
+        // Verifica se o CPF contém apenas dígitos numéricos
+        boolean somenteNumericos = !cpf.matches("[0-9]+");
+    
+        if (!formatoValido || somenteNumericos || cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+    
+        int[] digitos = new int[11];
+        for (int i = 0; i < 11; i++) {
+            digitos[i] = Character.getNumericValue(cpf.charAt(i));
+        }
+    
+        int soma1 = 0;
+        for (int i = 0; i < 9; i++) {
+            soma1 += digitos[i] * (10 - i);
+        }
+        int resto1 = soma1 % 11;
+        int digito1 = (resto1 < 2) ? 0 : 11 - resto1;
+    
+        int soma2 = 0;
+        for (int i = 0; i < 10; i++) {
+            soma2 += digitos[i] * (11 - i);
+        }
+    
+        int resto2 = soma2 % 11;
+        int digito2 = (resto2 < 2) ? 0 : 11 - resto2;
+    
+        return (digitos[9] == digito1) && (digitos[10] == digito2);
+    }
+    
 }
